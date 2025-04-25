fe0a2a6fb7b5ae70c9e9b0d5adb701e4f36cb5c6
sanjayababu2k01 <sanjayababu2001@gmail.com>
2025-04-25 22:09:54 +0530

    Add files via upload

---
+++ adding_comments.py
@@ -0,0 +1,11 @@
+#Sanjay 07/12/2024.4:50pm
+my_fav_num = 67
+message = f"My favorite number is {my_fav_num}"
+print(message)
+
+#Sanjay 07/12/2024.4:50pm
+x, y, z, j, i =2, 3, 5, 9, 1
+print(y+z)
+print(j-i)
+print(x*y+x)
+print(x**z/x/x)
